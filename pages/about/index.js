409c95a9c3664009ac8019e0d4b01a1bc514a101
Bondok6 <kirolousbondok6@gmail.com>
2022-11-12 01:44:56 +0200

    Finish Home/About desktop page

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,15 +1,86 @@
 const About = () => {
   return (
-    <section className="about">
-      <h2 className="heading-2">طريقك نحو الريادة</h2>
-      <div className="about__img-box">
-        <img src="/imgs/video.png" alt="video" />
-      </div>
-      <p className="about__text">
-        أن تكون جمعية رؤية الريادة عضوا قياديا في تطوير منظومة ريادة الاعمال
-        وتعزيز الابداع والابتكار وفق متطلبات التنمية.
-      </p>
-    </section>
+    <>
+      <section className="section about">
+        <h2 className="heading-2">طريقك نحو الريادة</h2>
+        <div className="about__img-box">
+          <img src="/imgs/video.png" alt="video" />
+        </div>
+        <p className="about__text">
+          أن تكون جمعية رؤية الريادة عضوا قياديا في تطوير منظومة ريادة الاعمال
+          وتعزيز الابداع والابتكار وفق متطلبات التنمية.
+        </p>
+      </section>
+
+      <section className="section">
+        <h2 className="heading-2">
+          قيم <span>الجمعية</span>
+        </h2>
+        <div className="values">
+          <img src="/imgs/second-section.PNG" alt="values" />
+        </div>
+      </section>
+
+      <section className="section">
+        <h2 className="heading-2">
+          أهداف <span>الجمعية</span>
+        </h2>
+        <div className="goals">
+          <img src="/imgs/third-section.PNG" alt="goals" />
+        </div>
+      </section>
+
+      <section className="section ambition">
+        <h2 className="heading-2">
+          نطمح بأن نكون في <span>2026</span>
+        </h2>
+        <ul className="ambition__list">
+          <li className="ambition__item">
+            <img src="/imgs/workshope.png" alt="" />
+            <h3 className="heading-3">حققنا 30 ورشة عمل</h3>
+          </li>
+          <li className="ambition__item">
+            <img src="/imgs/company.svg" alt="" />
+            <h3 className="heading-3">ساهمنا في تطوير 10 شركات </h3>
+          </li>
+          <li className="ambition__item">
+            <img src="/imgs/idea.svg" alt="" />
+            <h3 className="heading-3">احتضنا 20 فكرة ريادية</h3>
+          </li>
+          <li className="ambition__item">
+            <img src="/imgs/research.svg" alt="" />
+            <h3 className="heading-3">قدمنا 3 بحوث علمية </h3>
+          </li>
+          <li className="ambition__item">
+            <img src="/imgs/volunteer.svg" alt="" />
+            <h3 className="heading-3">خلقنا 30 فرصة تطوعية </h3>
+          </li>
+          <li className="ambition__item">
+            <img src="/imgs/effective.svg" alt="" />
+            <h3 className="heading-3">15 شراكة مجتمعيه فاعله </h3>
+          </li>
+          <li className="ambition__item">
+            <img src="/imgs/beneficiary.svg" alt="" />
+            <h3 className="heading-3">30 الف مستفيد من برامجنا </h3>
+          </li>
+        </ul>
+      </section>
+
+      <section className="section partners">
+        <h2 className="heading-2">شركائنا</h2>
+        <ul className="partners__container">
+          <li className="partner">
+            <img src="/imgs/partener_3.svg" alt="partner" />
+          </li>
+          <li className="partner">
+            <img src="/imgs/partener_2.svg" alt="partner" />
+          </li>
+          <li className="partner">
+            <img src="/imgs/partener_1.svg" alt="partner" />
+          </li>
+        </ul>
+      </section>
+    </>
   );
 };
 
