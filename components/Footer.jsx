ea4857095502179a80d83f0af7093c99abd76a68
Bondok6 <kirolousbondok6@gmail.com>
2022-11-11 22:27:40 +0200

    Finish Footer section

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,19 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <div className="footer__info">
+        <div className="footer__logo">
+          <img src="/imgs/logo.png" alt="logo" />
+        </div>
+        <p className="footer__text">
+          تحت إشراف المركز الوطني لتنمية القطاع غير الربحي
+        </p>
+      </div>
+      <div className="footer__copyRight">
+        <p>جميع الحقوق محفوظة &copy; 2022</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
