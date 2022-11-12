b8056f268205026431761bfc077f41db313da624
Bondok6 <kirolousbondok6@gmail.com>
2022-11-12 06:37:48 +0200

    Add Mobile Menu

--- components/navbar/Menu.jsx
+++ components/navbar/Menu.jsx
@@ -1,6 +1,51 @@
 import Link from 'next/link';
+import { useState } from 'react';
 
 const Menu = () => {
+  const [open, setOpen] = useState(false);
+
+  if (open) {
+    return (
+      <nav className="navbar-menu">
+        <ul className="navbar-menu__list" onClick={() => setOpen(false)}>
+          <img src="/imgs/logo.png" alt="logo" className="logo" />
+
+          <li className="navbar-menu__item">
+            <Link href="/" className="navbar-menu__link">
+              نبذة عنا
+            </Link>
+          </li>
+          <li className="navbar-menu__item">
+            <Link href="/program" className="navbar-menu__link">
+              برامجنا
+            </Link>
+          </li>
+          <li className="navbar-menu__item">
+            <Link
+              href="/regulations-and-policies"
+              className="navbar-menu__link"
+            >
+              اللوائح و السياسات
+            </Link>
+          </li>
+          <li className="navbar-menu__item">
+            <Link href="/volunteering" className="navbar-menu__link">
+              التطوع
+            </Link>
+          </li>
+          <li className="navbar-menu__item">
+            <Link href="/contact" className="navbar-menu__link">
+              تواصل معنا
+            </Link>
+          </li>
+        </ul>
+        <button onClick={() => setOpen(false)} className="navbar-menu__close">
+          &#128473;
+        </button>
+      </nav>
+    );
+  }
+
   return (
     <nav className="menu-nav">
       <ul className="menu-nav__list">
@@ -10,7 +55,7 @@ const Menu = () => {
           </Link>
         </li>
         <li className="menu-nav__item">
-          <div className="menu-icon">
+          <div className="menu-icon" onClick={() => setOpen(!open)}>
             <div className="menu-icon__line"></div>
             <div className="menu-icon__line"></div>
             <div className="menu-icon__line"></div>
